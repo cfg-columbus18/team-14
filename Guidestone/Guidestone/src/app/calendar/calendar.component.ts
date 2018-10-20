74cf72df6f9e74d1c63334096b672ed844bd5f3e
Forlefac Fontem <fontem.2@osu.edu>
2018-10-20 04:24:13 -0400

    update to code`

--- Guidestone/Guidestone/src/app/calendar/calendar.component.ts
+++ Guidestone/Guidestone/src/app/calendar/calendar.component.ts
@@ -5,15 +5,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
-  bad : boolean= false;
+  c1 : boolean= false;
+  c2 : boolean= false;
+  //new
+   c3 : boolean= false;
+   c4 : boolean= false;
+   c5 : boolean= false;
   constructor() {
-    window.onload = function() {
-          
-  };
-  }
+}
   ngOnInit() {
   }
+  reset(){
+    this.c1 = false;
+    this.c2 = false;
+    this.c3 = false;
+    this.c4 = false;
+    this.c5 = false;
+  }
   handleClick(){
-    this.bad = !this.bad;
+    this.reset();
+    this.c1 = !this.c1;
+  }
+  handleClick2(){
+    this.reset();
+    this.c2 = !this.c2;
+  }
+  handleClick3(){
+    this.reset();
+    this.c3 = !this.c3;
+  }
+  handleClick4(){
+    this.reset();
+    this.c4 = !this.c4;
+  }
+  handleClick5(){
+    this.reset();
+    this.c5 = !this.c5;
   }
 }
