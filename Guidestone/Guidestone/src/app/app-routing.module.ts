07e09ea713d199dc964ee5804ab74b6463d90c44
calebcodesgud <43999129+calebcodesgud@users.noreply.github.com>
2018-10-19 20:41:14 -0400

    routing working enough to dev pages
    simple routing

--- Guidestone/Guidestone/src/app/app-routing.module.ts
+++ Guidestone/Guidestone/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { HowFeelingPageComponent } from './how-feeling-page/how-feeling-page.component';
+import { WhatActivityPageComponent } from './what-activity-page/what-activity-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'first-view', component: FirstPageComponent},
+  {path: 'how-feeling', component: HowFeelingPageComponent},
+  {path: 'what-activity', component: WhatActivityPageComponent},
+  {path: '', redirectTo: '/first-view', pathMatch: 'full'},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
