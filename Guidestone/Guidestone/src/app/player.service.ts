e0238ac62a657463fb2636f23acdd4345c6fdb48
kamisch <chengtwv2@gmail.com>
2018-10-20 07:38:51 -0400

    added level system

--- Guidestone/Guidestone/src/app/player.service.ts
+++ Guidestone/Guidestone/src/app/player.service.ts
@@ -9,11 +9,13 @@ export class PlayerService {
   MindfulPoints:number; 
   PhysicalPoints:number; 
   SocialPoints:number;
+  level:number;
   constructor() { 
     this.XP = 0;
     this.MindfulPoints=0; 
     this.PhysicalPoints=0; 
     this.SocialPoints=0; 
+    this.level=0;
   }
 
   updateXP(value: number) {
@@ -53,13 +55,38 @@ export class PlayerService {
     updatePoints(num: number) {
       if (num == 1) {
         this.updatePhysicalPoints(15);
+        this.increase(15);
       } else if (num == 2) {
         this.updateMindfulPoints(15);
+        this.increase(25);
       } else if (num == 3) {
         this.updateSocialPoints(15);
+        this.increase(35);
       }
     }
+      
+  increase(value:number) {
+    var width = +(<HTMLInputElement>document.getElementById("myBar")).innerText;
 
+    if (width >= 100) {
+      width = 0;
+    } else {
+
+         width+= value;
+         if (width >= 100){
+           this.level += 1;
+           width = 100;
+         }
+       }
+      (<HTMLInputElement>document.getElementById("myBar")).innerText = width + '';
+           document.getElementById("myBar").style.width = width + '%';
+           document.getElementById("level").innerText = this.level.toString();
+           
+       
+  }
+  getLevel(){
+    return this.level;
+  }
   getXP() {
     return this.XP;
   }
